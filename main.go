6213a55810a497e392371d7ddb73e534acb023e9
SushmaKambam <sushmakambam@loyalistcollege.com>
2024-07-11 18:45:45 -0400

    Added an about end point

--- main.go
+++ main.go
@@ -10,4 +10,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the API Server!")
 	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is an About Page \n Group-A In Class Lab 4 \n")
+	})
 }
