f4e6d0f8cc9796459c69a380d36a1a15b122a8b5
Shivah99 <shivaji9712@gmail.com>
2024-07-11 18:57:31 -0400

    Comments in main.go

--- main.go
+++ main.go
@@ -6,14 +6,21 @@ import (
 )
 
 func main() {
-
+	// Define the handler function for the home endpoint ("/") page
+	// and return a message to the client
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the API Server!")
 	})
 
+	// Define the handler function for the about endpoint ("/about")
+
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "This is an About Page \n Group-A In Class Lab 4 \n")
 	})
+
+	// Start the server on port 8080
+	// Print a message to the console
+
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
