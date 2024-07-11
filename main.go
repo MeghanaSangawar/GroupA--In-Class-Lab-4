d61c8ee3acbb1dcf3fd41b9354058662d814ba0c
yaminikunapareddy <159596254+yaminikunapareddy@users.noreply.github.com>
2024-07-11 18:51:35 -0400

    starting the server

--- main.go
+++ main.go
@@ -14,4 +14,9 @@ func main() {
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "This is an About Page \n Group-A In Class Lab 4 \n")
 	})
+	fmt.Println("Server is running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
